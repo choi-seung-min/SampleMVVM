5e3d849a6bc69eb3aed35276c0625fbfde250598
seung min <3235tmdals@gmail.com>
2019-12-21 22:41:03 +0900

    calculator is working

--- app/src/main/java/com/example/samplemvvm/MainActivity.kt
+++ app/src/main/java/com/example/samplemvvm/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.samplemvvm
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.samplemvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val viewModel = MainViewModel()
+        binding.viewModel = viewModel
     }
 }
